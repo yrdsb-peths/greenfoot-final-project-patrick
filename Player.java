0059b2f2a6f6da4b9187410216f6b15a682a022e
Patrick Hu <348688052@gapps.yrdsb.ca>
2022-05-29 22:05:35 -0400

    Added animated knight as player

--- Player.java
+++ Player.java
@@ -12,12 +12,30 @@ public class Player extends MainObjects
     public int jumpHeight = -11;
     public int verticalSpeed = 0;
     public int acceleration = 1;
+    public int run_index = 0;
+    public boolean facingRight = false;
+    SimpleTimer animTimer = new SimpleTimer();
+    GreenfootImage[] runFacingRight = new GreenfootImage[10];
+    GreenfootImage[] runFacingLeft = new GreenfootImage[10];
+    
+    public Player() {
+        for (int i = 0; i <= 9; i++) {
+            int x = i + 1;
+            runFacingRight[i] = new GreenfootImage("./sprites/knight/Run (" + x + ").png");
+            runFacingRight[i].scale(60, 60);
+            runFacingLeft[i] = new GreenfootImage("./sprites/knight/Run (" + x + ").png");
+            runFacingLeft[i].scale(60, 60);
+            runFacingLeft[i].mirrorHorizontally();
+        }
+        setImage(runFacingLeft[0]);
+    }
     
     public void act()
     {
         checkMovement();
         checkFalling();
         checkOffMap();
+        
     }
     
     public void checkMovement() {
@@ -36,10 +54,14 @@ public class Player extends MainObjects
             }
         }
         if (Greenfoot.isKeyDown("d")) {
+            facingRight = true;
             move(speed);
+            runAnimate();
         }
         if (Greenfoot.isKeyDown("a")) {
+            facingRight = false;
             move(speed * -1);
+            runAnimate();
         }
         if (Greenfoot.isKeyDown("space") && isOnGround()) {
             verticalSpeed = jumpHeight;
@@ -62,6 +84,20 @@ public class Player extends MainObjects
         if (getY() == Level1.HEIGHT - 1) { // on the floor of the world
             Level1 world = (Level1) getWorld();
             world.gameOver();
+        }
+    }
+    
+    public void runAnimate() {
+        if (animTimer.millisElapsed() > 60) {
+            if (facingRight) {
+                setImage(runFacingRight[run_index]);
+            }
+            else {
+                setImage(runFacingLeft[run_index]);    
+            }
+            run_index++;
+            run_index %= 10;
+            animTimer.mark();
         }
     }
 }
